c53bc1cc13ef25546af2164444a8b4dd8599ba58
Shakurov Nail <codingpas@gmail.com>
2021-06-05 23:51:03 +0400

    @Solution Can Place Flowers

---
+++ src/main/kotlin/easy/CanPlaceFlowers.kt
@@ -0,0 +1,27 @@
+package easy
+
+fun main(args: Array<String>) {
+    println(canPlaceFlowers(intArrayOf(1, 0, 0, 0, 1), n = 1)) // true
+    println(canPlaceFlowers(intArrayOf(1, 0, 0, 0, 1), n = 2)) // false
+    println(canPlaceFlowers(intArrayOf(1, 0, 0, 0, 0, 1), n = 2)) // false
+    println(canPlaceFlowers(intArrayOf(1, 0, 0, 0, 1, 0, 0), 2)) // true
+    println(canPlaceFlowers(intArrayOf(0, 0, 1, 0, 0), 1)) // true
+}
+
+fun canPlaceFlowers(flowerbed: IntArray, n: Int): Boolean {
+    var count = n
+    val list = flowerbed.toMutableList()
+    if (n == 0) return true
+    list.forEachIndexed { index, value ->
+        if ((list.getOrNull(index - 1) == 0 || list.getOrNull(index - 1) == null)
+            && (list.getOrNull(index) == 0 && (list.getOrNull(index + 1) == 0 || list.getOrNull(index + 1) == null))
+        ) {
+            list[index] = 1
+            count -= 1
+            if (count == 0) {
+                return true
+            }
+        }
+    }
+    return count == 0
+}
