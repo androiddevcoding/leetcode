f9e294424f6676c149a37cd77a8df57b90b42185
Nail Shakurov <codingpas@gmail.com>
2023-02-26 22:23:28 +0400

    @Solution 2 task

--- src/main/kotlin/easy/FindAllNumbersDisappearedArray.kt
+++ src/main/kotlin/easy/FindAllNumbersDisappearedArray.kt
@@ -1,24 +1,31 @@
 package easy
 
+
 /**
-Link: https://leetcode.com/problems/missing-number/
+Link: https://leetcode.com/problems/find-all-numbers-disappeared-in-an-array/
  **/
 fun main(args: Array<String>) {
-    println(findDisappearedNumbers(intArrayOf(4,3,2,7,8,2,3,1)))
-    println(findDisappearedNumbers(intArrayOf(1,1)))
-    println(findDisappearedNumbers(intArrayOf(9, 6, 4, 2, 3, 5, 7, 0, 1)))
+    println(findDisappearedNumbers(intArrayOf(4, 3, 2, 7, 8, 2, 3, 1)))
+    println(findDisappearedNumbers(intArrayOf(1, 1)))
 }
 
 fun findDisappearedNumbers(nums: IntArray): List<Int> {
     var i = 0
-    while(i < nums.size + 1){
+    while (i < nums.size) {
         val pos = nums[i] - 1
-        if(nums[i] != nums[pos]) {
-            val value  = nums[i]
+        if (nums[i] != nums[pos]) {
+            val value = nums[i]
             nums[i] = nums[pos]
             nums[pos] = value
-        } else i+=1
+        } else i += 1
     }
-    print(nums)
-    return emptyList()
+    //print(nums.contentToString())
+    val miss = mutableListOf<Int>()
+    for (i in 0..nums.size - 1) {
+        if (nums[i] != i + 1) {
+            miss.add(i + 1)
+        }
+    }
+
+    return miss
 }
