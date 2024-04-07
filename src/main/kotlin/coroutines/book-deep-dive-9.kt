293dc6f250cfb573c8fedb68c6ec8bab49a72052
Nail Shakurov <codingpas@gmail.com>
2024-04-07 22:10:21 +0400

    @Solution

---
+++ src/main/kotlin/coroutines/book-deep-dive-9.kt
@@ -0,0 +1,27 @@
+package coroutines
+
+import kotlinx.coroutines.*
+import kotlinx.coroutines.channels.ReceiveChannel
+import kotlinx.coroutines.selects.select
+
+suspend fun requestData1(): String {
+    delay(100_000)
+    return "Data1"
+}
+suspend fun requestData2(): String {
+    delay(1000)
+    return "Data2"
+}
+suspend fun askMultipleForData(): String = coroutineScope { select<String> {
+    async { requestData1() }.onAwait { it }
+    async { requestData2() }.onAwait { it }
+}.also { coroutineContext.cancelChildren() }
+}
+
+suspend fun main(): Unit = coroutineScope {
+
+    println(askMultipleForData())
+}
+
+
+
