6dd4425ded93d8f24e46cc2cec2cca4ba176ebd5
Nail Shakurov <codingpas@gmail.com>
2024-04-09 19:05:04 +0400

    @Solution

--- src/main/kotlin/coroutines/book-deep-dive-10.kt
+++ src/main/kotlin/coroutines/book-deep-dive-10.kt
@@ -1,21 +1,15 @@
 package coroutines
 
 import kotlinx.coroutines.*
-import kotlin.concurrent.thread
 
-suspend fun main(): Unit = coroutineScope {
-    runBlocking {
-        this.launch { // same as just launch
-            delay(1000L)
-            println("World!")
-        }
-        launch { // same as this.launch
-            delay(2000L)
-            println("World!")
-        }
-        println("Hello,")
+
+suspend fun main(): Unit =  runBlocking {
+    launch(Job()) { // the new job replaces one from parent
+        delay(1000)
+        println("Will not be printed")
     }
 }
+
 
 
 
