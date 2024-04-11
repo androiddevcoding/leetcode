aef466884ff236a9fe9d72a812cd919d756ca440
Nail Shakurov <codingpas@gmail.com>
2024-04-11 23:10:37 +0400

    @Solution

--- src/main/kotlin/coroutines/book-deep-dive-10.kt
+++ src/main/kotlin/coroutines/book-deep-dive-10.kt
@@ -1,23 +1,35 @@
 package coroutines
 
-import kotlinx.coroutines.*
+import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withTimeout
+
+
+suspend fun test(): Int = withTimeout(1500) {
+    delay(1000)
+    println("Still thinking")
+    delay(1000)
+    println("Done!")
+    42
+}
 
 
 suspend fun main(): Unit = coroutineScope {
-    val job = Job()
-    launch(job) {
-            do {
-                Thread.sleep(200)
-                println("Printing") } while (isActive)
-
+    launch { // 1
+        launch { // 2, cancelled by its parent
+            delay(2000)
+            println("Will not be printed")
+        }
+        withTimeout(1000) { // we cancel launch
+            delay(1500)
+        }
     }
-    delay(1000)
-    job.cancelAndJoin()
-    println("Cancelled successfully")
-    delay(1000)
-
+    launch { // 3
+        delay(2000)
+        println("Done")
+    }
 }
-
 
 
 
