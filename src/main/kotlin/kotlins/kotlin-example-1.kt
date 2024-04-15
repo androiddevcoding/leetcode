9f2b061bb6ac7fc0a5039348213598520a681474
Nail Shakurov <codingpas@gmail.com>
2024-04-15 15:09:14 +0400

    @Solution

---
+++ src/main/kotlin/kotlins/kotlin-example-1.kt
@@ -0,0 +1,12 @@
+package kotlins
+
+import kotlinx.coroutines.*
+import kotlinx.coroutines.awaitAll
+import kotlinx.coroutines.flow.*
+import java.io.File
+
+
+suspend fun main(): Unit = coroutineScope {
+
+}
+
